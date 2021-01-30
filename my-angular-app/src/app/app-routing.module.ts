0b30971b40b50ef22e191b8561d43a8a90bd2374
Nikolay <kurenkov.mykola@ukr.net>
2021-01-30 20:22:49 +0200

    Navigation through links

--- my-angular-app/src/app/app-routing.module.ts
+++ my-angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BeachesComponent } from './beaches/beaches.component';
+import { BlogComponent } from './blog/blog.component';
+import { FoodComponent } from './food/food.component';
+import { GirlsComponent } from './girls/girls.component';
+import { HistoryComponent } from './history/history.component';
+import { HomeComponent } from './home/home.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PlacesComponent } from './places/places.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "blog",
+    component: BlogComponent
+  },
+  {
+    path: "photos",
+    component: PhotosComponent
+  },
+  {
+    path: "girls",
+    component: GirlsComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "about/history",
+    component: HistoryComponent
+  },
+  {
+    path: "about/places",
+    component: PlacesComponent
+  },
+  {
+    path: "about/beaches",
+    component: BeachesComponent
+  },
+  {
+    path: "about/food",
+    component: FoodComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
